3106e7e89f1970d6cea18b00bc8875959236a42f
muhammad rayandika <mrayandika15@gmail.com>
2021-11-18 23:56:21 +0700

    add(file)

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dc9617c3097ead22e6a8eadeaba94ab5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a3dbea90-8432-44d8-8d90-1481c47f0f30'
+};
